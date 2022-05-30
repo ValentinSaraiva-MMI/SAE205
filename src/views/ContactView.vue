03078563d06c01ae4dca7e83cd43df4c9b1b1979
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2022-05-31 00:28:36 +0200

    installation plugins forms

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,21 @@
 <template>
-    <p>Contact</p>
+<div class="bg-noir">
+
+
+
+<div class="w-full  relative">
+  <img
+    src="../../public/images/contact/hero_contact.png"
+    class="w-auto h-auto "
+  />
+  <p
+    class="  xl:text-8xl lg:text-6xl xl:pb-4  absolute left-[16.41px] top-[39.34px] text-[33.34638595581055px] font-medium text-left text-[#d4cebd]"
+  >
+    NOUS CONTACTER
+  </p>
+
+</div>
+</div>   
 </template>
 
 

494a1a445fcd8cd713b2d271e4989c8fa2d6588c
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2022-05-30 23:33:50 +0200

    image souvenir mise

--- src/views/FestivalView.vue
+++ src/views/FestivalView.vue
@@ -110,35 +110,60 @@ job="lol"
 
 
 <div class="container mx-auto">
-    <div class="grid-cols-3 p-20 space-y-2 bg-yellow-200 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+    <div class="grid-cols-3 p-20 space-y-2 bg-noir lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+       
         <div class="w-full rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            <img src="public\images\souvenires\maxime-lebrun-h5G0WdtwcEg-unsplash 1.png"
                 alt="image">
         </div>
-        <div class="w-full col-span-2 row-span-2 rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+        <div class="w-full  rounded">
+            <img src="public\images\souvenires\pexels-mantas-hesthaven-354304 1.png"
                 alt="image">
         </div>
+          <div class="w-full rounded">
+            <img src="public\images\souvenirs2\3.png"
+                alt="image">
+        </div>
+
+   <div class="w-full  rounded">
+            <img src="public\images\souvenirs2\1.png"
+                alt="image">
+        </div>
+        <div class="w-full  rounded">
+            <img src="public\images\souvenirs2\7.png"
+                alt="image">
+        </div>
+        
+          <div class="w-full rounded">
+            <img src="public\images\souvenirs2\4.png"
+                alt="image">
+        </div>
+        
+
         <div class="w-full rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            <img src="public\images\souvenirs2\5.png"
                 alt="image">
         </div>
-        <div class="w-full rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+          <div class="w-full  rounded">
+            <img src="public\images\souvenires\image 28.png"
                 alt="image">
         </div>
-        <div class="w-full rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+
+        
+         <div class="w-full rounded ">
+            <img src="../../public/images/souvenires/erik-mclean-zgSC_LEWw20-unsplash 1.png"
                 alt="image">
         </div>
-        <div class="w-full rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+
+        <div class="w-full rounded col-span-2 row-span-1">
+            <img src="public\images\souvenirs2\6.png"
                 alt="image">
         </div>
-         <div class="w-full col-span-2 row-span-1 rounded">
-            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                alt="image">
-        </div>
+
+        
+         
+      
+      
     </div>
 </div>
 

f3a8cf9b4a366b89143d6bf9bfe9cf93da42201c
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2022-06-02 10:12:17 +0200

    liste simple synchronisées

--- src/views/ArtisteDynamiqueView.vue
+++ src/views/ArtisteDynamiqueView.vue
@@ -3,21 +3,22 @@
     <h5 class>Liste des artistes - Simple liste</h5>
   </div>
   <div v-for="Artistes in listeArtistes" :key="Artistes.id">
-    <p>{{ artistes.nom }}</p>
+    <p>{{ Artistes.nom }}</p>
+    <img :src="Artistes.image" alt="">
   </div>
   <hr />
 </template>
 
 
 <script>
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
 
 export default {
   data() {
     return {
-      nom: null,
-      message: null,
       listeArtistes: [],
+      
     };
   },
   mounted() {
@@ -27,30 +28,28 @@ export default {
     async getArtistes() {
       const firestore = getFirestore();
       const dbArtistes = collection(firestore, "Artistes");
-      const query = await getDocs(dbArtistes);
-      query.forEach((doc) => {
-        let Artistes = {
+      const query = await onSnapshot(dbArtistes, (snapshot) => {
+        console.log("query", query);
+        this.listeArtistes = snapshot.docs.map((doc) => ({
           id: doc.id,
-          nom: doc.data().nom,
-        };
-        this.listeArtistes.push(Artistes);
+          ...doc.data(),
+        }));
+        this.listeArtistes.forEach(function (personne) {
+          const storage = getStorage();
+          const spaceRef = ref(storage, "Artistes/" + personne.image);
+          getDownloadURL(spaceRef)
+            .then((url) => {
+              personne.image = url;
+              console.log("personne", personne);
+            })
+            .catch((error) => {
+              console.log("erreur downloadUrl", error);
+            });
+        });
+        console.log("listeArtistes", this.listeArtistes);
       });
     },
   },
-};
-</script>
+  };
 
-<style scoped>
-.center {
-  text-align: center;
-}
-.title {
-  font-size: 1.4rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: #34495e;
-}
-h4 {
-  font-weight: bold;
-}
-</style>
+</script>
